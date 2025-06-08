c8caee52d97f5c4efb519cb68db03dec2fdeee24
Devesh Kumar Sharma <deveshkumarsharma1999@gmail.com>
2025-06-08 12:36:55 +0530

    Add files via upload

---
+++ Online examinattion system.sql
@@ -0,0 +1,76 @@
+create database exam_system;
+use exam_system
+
+CREATE TABLE `answers` (
+  `answer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `exam_id` int(11) NOT NULL,
+  `question` varchar(45) NOT NULL,
+  `answer` varchar(45) NOT NULL,
+  `correct_answer` varchar(45) NOT NULL,
+  `status` varchar(45) NOT NULL,
+  PRIMARY KEY (`answer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `courses` (
+  `course_name` varchar(25) NOT NULL,
+  `total_marks` int(11) NOT NULL,
+  `time` varchar(45) NOT NULL,
+  PRIMARY KEY (`course_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `exams` (
+  `exam_id` int(11) NOT NULL AUTO_INCREMENT,
+  `std_id` varchar(45) NOT NULL,
+  `course_name` varchar(45) NOT NULL,
+  `total_marks` varchar(45) NOT NULL,
+  `obt_marks` varchar(45) DEFAULT NULL,
+  `date` varchar(45) NOT NULL,
+  `start_time` varchar(45) NOT NULL,
+  `end_time` varchar(45) DEFAULT NULL,
+  `exam_time` varchar(45) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`exam_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `questions` (
+  `question_id` int(11) NOT NULL AUTO_INCREMENT,
+  `course_name` varchar(45) NOT NULL,
+  `question` varchar(255) NOT NULL,
+  `opt1` varchar(85) NOT NULL,
+  `opt2` varchar(85) NOT NULL,
+  `opt3` varchar(85) NOT NULL,
+  `opt4` varchar(85) NOT NULL,
+  `correct` varchar(85) NOT NULL,
+  PRIMARY KEY (`question_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `test` (
+  `name` varchar(25) NOT NULL,
+  `age` int(11) DEFAULT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `user_name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `user_type` varchar(45) NOT NULL,
+  `contact_no` varchar(45) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='		';
+
+
+
+
+
+
+
+
+
+
+
